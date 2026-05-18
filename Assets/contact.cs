439248243cd7fe2b4d3611c9a2264cef862c222b
Yudai Tehira <241205118@ccmailg.meijo-u.ac.jp>
2026-05-18 12:11:44 +0900

    Update contact.cs

--- Assets/contact.cs
+++ Assets/contact.cs
@@ -8,18 +8,48 @@ public class contact : MonoBehaviour
 {
 
     [SerializeField] private TextMeshProUGUI ContactCount;
+    [SerializeField] private TextMeshProUGUI CheckCount;
+    [SerializeField] private GameObject player;
 
     private int count;
+    private int check;
+    private Vector3 savedPosition;
+    private bool isChecked;
+    private float holdTime;
+    private float needHoldTime;
+
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
+        check = 0;
+        isChecked = false;
+        holdTime = 0f;
+        needHoldTime = 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
         ContactCount.text = string.Format("Hit wall {0}", count);
+        CheckCount.text = string.Format("check {0}", check);
+
+        if(Input.GetKey(KeyCode.RightShift))
+        {
+            holdTime += Time.deltaTime;
+            if(holdTime >= needHoldTime)
+            {
+                if(isChecked)
+                {
+                    player.transform.position = savedPosition;
+                    holdTime = 0f;
+                }
+            }
+        }
+        else
+        {
+            holdTime = 0f;
+        }
     }
 
     void OnCollisionEnter(Collision other) // 衝突を判定する関数を呼ぶ 
@@ -28,5 +58,11 @@ public class contact : MonoBehaviour
         {
             count += 1; // 衝突フラグを上げる
         } 
+        if(other.gameObject.name == "checkpoint")
+        {
+            check = 1;
+            savedPosition = new Vector3(3f,1f,-7f);
+            isChecked = true;
+        }
     }
 }
